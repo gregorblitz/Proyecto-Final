584777dbfff6d29c0fe81ce64bb4b2301621c340
gregorblitz <61234405+gregorblitz@users.noreply.github.com>
2026-04-16 07:07:37 -0500

    El monstruo al atacar baja a la barra de vida
    El monstruo al atacar baja a la barra de vida

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.AI;
+using System.Collections; // NUEVO: Necesario para usar Corrutinas (tiempos de espera)
 
 public class Monster : MonoBehaviour
 {
@@ -8,12 +9,25 @@ public class Monster : MonoBehaviour
     public float detectDistance = 10f; // Distancia deteccion
     public float chaseSpeed = 5f;    // Velocidad con la que corre
 
+    [Header("Tiempos de Animación")]
+    public float roarDuration = 2f; // Segundos que se queda quieto rugiendo
+
+    [Header("Configuración de Ataque")]
+    public float attackDamage = 15f; // Cantidad de vida que quita por golpe
+    public float attackCooldown = 2f; // Segundos que tarda en volver a golpear
+    private float lastAttackTime = 0f; // Controla cuándo fue el último golpe
+
     private NavMeshAgent agent;
-    private bool isChasing = false;
+    private Animator animator; // Creacion de la variable para el cerebro de animaciones
+    // Lógica de estados
+    private bool hasDetectedPlayer = false;
+    private bool isRunning = false;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        //Busca el componente animator en los objetos hijos del monstruo
+        animator = GetComponentInChildren<Animator>();
         agent.speed = chaseSpeed;
     }
 
@@ -22,17 +36,60 @@ public class Monster : MonoBehaviour
         // Calcula la distancia entre el monstruo y el jugador
         float distance = Vector3.Distance(transform.position, player.position);
 
-        // Si el jugador entra en el rango, empieza la persecucion
-        if (distance <= detectDistance)
+        // Si el jugador entra en el rango y no lo ha detectado
+        if (distance <= detectDistance && hasDetectedPlayer == false)
         {
-            isChasing = true;
+            hasDetectedPlayer = true; // Activa deteccion del jugador
+            // Inicia la secuencia de Rugir y luego Correr
+            StartCoroutine(RoarThenChase());
         }
-        //Si esta en modo persecucion, le dice al agente que vaya hacia el jugador
-        if (isChasing)
+        // Logica de persecución ocurre despues del rugido
+        if (isRunning == true)
         {
-            // El monstruo actualiza su destino a la posición del jugador constantemente
+            // Persigue al jugador
             agent.SetDestination(player.position);
+
+            // Lógica de Ataque
+            // Si el monstruo llega al stopping distance ataca.
+            // Le suma 0.2f como margen de error para que no falle al calcular.
+            if (distance <= agent.stoppingDistance + 0.2f)
+            {
+                // cuando ya pasa el tiempo de recarga desde el ultimo ataque
+                if (Time.time >= lastAttackTime + attackCooldown)
+                {
+                    animator.SetTrigger("Attack"); // Dispara el golpe
+
+                    // Busca el script de danna en el jugador y le quita vida
+                    PlayerStatus pStatus = player.GetComponent<PlayerStatus>();
+                    if (pStatus != null)
+                    {
+                        // Le resta para hacer daño a la barra de salud
+                        pStatus.ModifyHealth(-attackDamage);
+                        Debug.Log("El monstruo ataca ", pStatus);
+                    }
+
+                    // Reinicia cronometro de ataque
+                    lastAttackTime = Time.time;
+                }
+            }
         }
+    }
+
+    // Corrutina que controla el tiempo para rugir y luego correr
+    IEnumerator RoarThenChase()
+    {
+        // Frena fisicamente al monstruo para que no se deslice ni se mueva
+        agent.isStopped = true;
+
+        // Dispara la animacion -- pasa de sniff a roar
+        animator.SetBool("isChasing", true);
+
+        // Espera exactamente los segundos que dura el rugido
+        yield return new WaitForSeconds(roarDuration);
+
+        // Termina el tiempo. Permite correr
+        agent.isStopped = false;
+        isRunning = true; // Activa la parte del Update que mueve al NavMesh
     }
 
     // para ver el círculo de detección en el editor de Unity
