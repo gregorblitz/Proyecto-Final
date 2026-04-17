e90ba5a9868ab9f058fdc4c02017f8bdd2268cc5
gregorblitz <61234405+gregorblitz@users.noreply.github.com>
2026-04-17 12:07:55 -0500

    Mecanica de correr y atacar (frenar cuando ataca)
    Mecanica de correr y atacar (frenar cuando ataca)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,12 @@ public class PlayerController : MonoBehaviour
 {
     public InputActionAsset inputActions;
     //acciones
-    private InputAction moveAction;
+    private InputAction moveAction; //Accion para mover
     private InputAction lookAction;
-    private InputAction jumpAction;
-    private InputAction crawlAction;
+    private InputAction jumpAction; //Accion para saltar
+    private InputAction crawlAction;//Accion para arrastrarse 
+    private InputAction runAction; // Accion para correr
+    private InputAction attackAction; // Accion para atacar
 
     private Vector2 moveAnim;
     private Vector2 lookAnim;
@@ -17,18 +19,27 @@ public class PlayerController : MonoBehaviour
     private CapsuleCollider capsuleCollider; // Para saber la altura del jugador
      //***Constantes***
     
-    [Header("Configuración de movimiento")]
+    [Header("Variables movimiento")]
     public float walkSpeed = 5;
     public float rotateSpeed = 150;
     public float jumpSpeed = 5;
+    public float runSpeed = 7; // Velocidad al correr
 
     // Configuracion de la cápsula al gatear
-    [Header("Configuracion de gateo")]
+    [Header("Variables gateo")]
     public float crawlHeight = 1f; // Altura al gatear
+
+    [Header("Variables Ataque")]
+    public float attackCooldown = 1.2f; // Tiempo entre cada golpe
+    public float freezeDuration = 0.8f; // Tiempo de congelacion pies luego de atacar
+    private float nextAttackTime = 0f;  // Reloj interno
+    private float unfreezeTime = 0f;    // Reloj interno de descongelamiento
+
     // Variables para la memoria del tamaño
     private float originalHeight;
     private Vector3 originalCenter;
     private bool isCrawling = false;
+    private bool isRunning = false; // Variable para saber si corre
 
     private void Awake()
     {
@@ -38,10 +49,12 @@ public class PlayerController : MonoBehaviour
         var playerMap = inputActions.FindActionMap("Player");
         //
         //***Se lee lo del mapa local en lugar del global
-        moveAction = playerMap.FindAction("Move");
+        moveAction = playerMap.FindAction("Move");   // Busca la accion Move
         lookAction = playerMap.FindAction("Look");
-        jumpAction = playerMap.FindAction("Jump");
-        crawlAction = playerMap.FindAction("Crawl");
+        jumpAction = playerMap.FindAction("Jump");   // Busca la accion Jump
+        crawlAction = playerMap.FindAction("Crawl"); // Busca la accion Crawl
+        runAction = playerMap.FindAction("Sprint");  // Busca la accion Sprint
+        attackAction = playerMap.FindAction("Attack"); // Busca accion Attack
 
         // Guarda referencia del Rigidbody 
         rigidbody = GetComponent<Rigidbody>();
@@ -79,6 +92,7 @@ public class PlayerController : MonoBehaviour
         moveAnim = moveAction.ReadValue<Vector2>();
         // isCrawling es true si se oprime el botón
         isCrawling = crawlAction.IsPressed();
+        isRunning = runAction.IsPressed(); // Lee si tiene presionado Shift
 
         // Modificacion fisica de la capsula si esta gateando
         if (isCrawling)
@@ -97,10 +111,23 @@ public class PlayerController : MonoBehaviour
         // Le pasa estado final al Animator
         animator.SetBool("isCrawling", isCrawling);
 
+        // No salta si esta congelado por atacar
+        bool isFrozen = Time.time < unfreezeTime;
+
         //**Solo salta si se oprime el boton y si esta tocando el suelo y si no esta gateando
         if (jumpAction.WasPressedThisFrame() && IsGrounded() && !isCrawling)
         {
             Jump();
+        }
+
+        // ***ATAQUE***
+        // Presiona el boton && paso el tiempo para siguiente ataque && no esta gateando
+        if (attackAction.WasPressedThisFrame() && Time.time >= nextAttackTime && !isCrawling)
+        {
+            animator.SetTrigger("Attack"); // Dispara la animación
+            nextAttackTime = Time.time + attackCooldown; // Reinicia reloj para siguiente ataque
+            // Congela los pies hasta dentro de 0.8s
+            unfreezeTime = Time.time + freezeDuration;
         }
     }
     
@@ -134,8 +161,13 @@ public class PlayerController : MonoBehaviour
     }
     private void Walking()
     {
-
-         //Guarda el input original (que presiona el jugador en W o S)
+        // Si el tiempo no supera al de descongelamiento se apaga el movimiento.
+        if (Time.time < unfreezeTime)
+        {
+            animator.SetFloat("Speed", 0); // Le dice a la animacion --> piernas quietas
+            return; // corta aqui y lo demas no se ejecuta
+        }
+        //Guarda el input original (que presiona el jugador en W o S)
         float inputY = moveAnim.y;
 
         // Calcula la velocidad
@@ -146,6 +178,15 @@ public class PlayerController : MonoBehaviour
         {
            // Si está agachado (ya sea por el botón o por el techo), camina a la mitad de velocidad
             currentSpeed = walkSpeed * 0.5f;
+        }
+        // si esta corriendo y se presiona W o S
+        else if (isRunning && inputY > 0)
+        {
+            // Solo permite correr si no esta agachado y va hacia adelante (inputY > 0)
+            currentSpeed = runSpeed;
+            
+            // Fuerza el inputY a 2 para activar la animacion de correr
+            inputY = 2f; 
         }
         //Aplica la animación y el movimiento con las nuevas variables 
         animator.SetFloat("Speed", inputY);
