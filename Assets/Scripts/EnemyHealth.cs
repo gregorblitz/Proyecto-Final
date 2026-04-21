f4c26485929a5e805378774ced5e903cfa18a577
gregorblitz <61234405+gregorblitz@users.noreply.github.com>
2026-04-21 15:10:13 -0500

    arreglo monstruo clonado por locura y resta de locura al matar monstruos
    arreglo monstruo clonado por locura y resta de locura al matar monstruos

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -6,9 +6,15 @@ public class EnemyHealth : MonoBehaviour
     public float maxHealth = 100f;
     private float currentHealth;
 
+    // Evita que el monstruo muera dos veces seguidas
+    private bool isDead = false;
+
     // Referencia a la barra visual
     [Header("Interfaz UI")]
     public Slider healthBar;
+
+    [Header("Recompensas")]
+    public float madnessReduction = -20f; // cantidad de locura que quita al matar monstruos
 
     private void Start()
     {
@@ -25,6 +31,10 @@ public class EnemyHealth : MonoBehaviour
     // Se llamara cuando la pica lo golpee
     public void TakeDamage(float damageAmount)
     {
+
+        // Si el monstruo ya está muerto, ignoramos los golpes extra
+        if (isDead) return;
+
         currentHealth -= damageAmount;
         Debug.Log("Monstruo recibio " + damageAmount + " de daño. Vida restante: " + currentHealth);
 
@@ -36,6 +46,7 @@ public class EnemyHealth : MonoBehaviour
 
         if (currentHealth <= 0)
         {
+            isDead = true; // Lo marca como muerto
             Die();
         }
     }
@@ -43,6 +54,19 @@ public class EnemyHealth : MonoBehaviour
     private void Die()
     {
         Debug.Log("Monstruo derrotado");
+
+        // Busca al jugador por su Tag para darle la recompensa
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            PlayerStatus pStatus = player.GetComponent<PlayerStatus>();
+            if (pStatus != null)
+            {
+                // Le pasamos el -20 a la funcion player status
+                pStatus.ModifyMadness(madnessReduction);
+                Debug.Log("El jugador recupera 20 de cordura");
+            }
+        }
         
         // Poner animacion de muerte
         // Destruir por el momento
